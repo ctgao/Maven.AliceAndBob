cffea18fa7969b0feb23db6ca5079dc0412627fd
Christine <christine@Zipcoders-MacBook-Pro-51.local>
2023-06-28 17:35:28 -0400

    alice and bob are ready to be greeted

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,27 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+        // set up scanner to get user input
+        Scanner in = new Scanner(System.in);
+        String yourName = null;
 
+        // get user input
+        System.out.print("What is your name? ");
+        yourName = in.nextLine();
+
+        // check and greet ONLY "Alice" and "Bob"
+        yourName = yourName.trim();
+        if(yourName.equalsIgnoreCase("Alice")){
+            System.out.println("Welcome! You're Alice? The one from the Looking Glass?");
+        }
+        else if(yourName.equalsIgnoreCase(("Bob"))){
+            System.out.println("Welcome Bob! Do you perhaps build things?");
+        }
     }
 }
